a62dcf03c046636a3da5158ee460526eaec3d0df
Bakuta Andrey <dra1n86@gmail.com>
2013-06-24 22:58:19 +0300

    KO stuff.

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
+require 'sinatra/content_for'
 
 module KnockoutShowcase
   class App < Sinatra::Base
     set :public_folder, 'public'
+
+    helpers Sinatra::ContentFor
 
     get "/products/:version/:framework" do
       erb :"#{params[:version]}/#{params[:framework]}"
---
+++ app/public/js/ko.js
@@ -0,0 +1,35 @@
+(function () {
+  var data = [
+    {
+      title: 'Xbox One',
+      description: 'Be first to experience Xbox One. The Day One Edition features a commemorative controller and an exclusive achievement.',
+      price: '$499',
+      quantity: 1,
+      img: '/images/consoles/xbox.png'
+    },
+    {
+      title: 'PlayStation®4',
+      description: 'The PlayStation®4 system redefines rich and immersive gameplay with powerful graphics and speed.',
+      price: '$399',
+      quantity: 1,
+      img: '/images/consoles/ps.png'
+    },
+    {
+      title: 'Wii U',
+      description: "It's the console that will singlehandedly change the way people play games for years to come.",
+      price: '$300',
+      quantity: 1,
+      img: '/images/consoles/wii.png'
+    }
+  ]
+
+
+
+  function CheckoutViewModel(data) {
+    var self = this;
+
+    ko.mapping.fromJS({ products: data }, {}, self);
+  }
+
+  ko.applyBindings(new CheckoutViewModel(data));
+})();
