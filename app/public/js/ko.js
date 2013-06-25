8728fb1d756522230c2353fe555247ebcf0bae1b
Andrey Bakuta <dra1n86@gmail.com>
2013-06-25 10:41:52 +0300

    Added formattedPrice.

--- app/public/js/ko.js
+++ app/public/js/ko.js
@@ -3,32 +3,46 @@
     {
       title: 'Xbox One',
       description: 'Be first to experience Xbox One. The Day One Edition features a commemorative controller and an exclusive achievement.',
-      price: '$499',
+      price: 499,
       quantity: 1,
       img: '/images/consoles/xbox.png'
     },
     {
       title: 'PlayStation®4',
       description: 'The PlayStation®4 system redefines rich and immersive gameplay with powerful graphics and speed.',
-      price: '$399',
+      price: 399,
       quantity: 1,
       img: '/images/consoles/ps.png'
     },
     {
       title: 'Wii U',
       description: "It's the console that will singlehandedly change the way people play games for years to come.",
-      price: '$300',
+      price: 300,
       quantity: 1,
       img: '/images/consoles/wii.png'
     }
   ]
 
 
-
   function CheckoutViewModel(data) {
+    var mapping = {
+      products: {
+        create: function(options) { return new ProductViewModel(options.data); }
+      }
+    }
+
+    ko.mapping.fromJS({ products: data }, mapping, this);
+  }
+
+
+  function ProductViewModel(data) {
     var self = this;
 
-    ko.mapping.fromJS({ products: data }, {}, self);
+    ko.mapping.fromJS(data, {}, this);
+
+    this.formattedPrice = ko.computed(function() {
+      return '$' + self.price();
+    });
   }
 
   $(function() {
